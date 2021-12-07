93b7a53a3fe0cd8f5b6929c47f3d34d89822377a
MurrayBaker <sambaker@f2s.com>
2021-12-07 16:34:01 +0000

    Day 7 part 2

--- adventofcode/src/main/scala/Day7.scala
+++ adventofcode/src/main/scala/Day7.scala
@@ -1,6 +1,23 @@
 import io.Source
+import util.control.Exception.allCatch
+import io.StdIn.readLine
+
+def unknownPartMessage = "I don't know what you mean"
 
 def day7: Unit = {
+    println("Part 1 or 2?")
+    val part = allCatch.opt(readLine().toInt);
+    part match {
+        case Some(p) => p match {
+            case 1 => day7Part1
+            case 2 => day7Part2
+            case _ => println(unknownPartMessage)
+        }
+        case None => println(unknownPartMessage)
+    }
+}
+
+def day7Part1: Unit = {
     val startingState = Source.fromResource("day7input.txt").getLines().next();
 
     val crabs = startingState
@@ -21,3 +38,28 @@ def day7: Unit = {
 
 def calculateFuelRequired(crabs : Array[Int], position : Int): Int = 
     crabs.map(c => Math.abs(c - position)).sum
+
+def calculateFuelRequiredHarmonic(crabs : Array[Int], position : Int): Int = 
+    crabs.map(c => getTriangularNumber(Math.abs(c - position))).sum
+
+def getTriangularNumber(input : Int): Int = 
+    (input * (input + 1)) / 2
+
+def day7Part2: Unit = {
+    val startingState = Source.fromResource("day7input.txt").getLines().next();
+
+    val crabs = startingState
+        .split(",")
+        .map(crab => {
+            intParse(crab) match {
+                case Some(i) => i
+                case None => 0
+            }
+        })
+
+    val fuelValues = (0 to crabs.max).map(i => {
+         calculateFuelRequiredHarmonic(crabs, i)
+    })
+
+    println(fuelValues.min)
+}
